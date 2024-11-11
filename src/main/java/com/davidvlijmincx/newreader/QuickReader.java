bc4fd7bfe4e2840f8717640eaa4cd1bf8a8ab7bc
david <david.vlijmincx@gmail.com>
2024-11-11 21:41:38 +0100

    Added new file setup

--- src/main/java/com/davidvlijmincx/newreader/QuickReader.java
+++ src/main/java/com/davidvlijmincx/newreader/QuickReader.java
@@ -3,14 +3,34 @@ package com.davidvlijmincx.newreader;
 import com.davidvlijmincx.setup.BenchmarkFiles;
 import com.davidvlijmincx.setup.FileTooReadData;
 
+import java.lang.foreign.MemorySegment;
+
 public class QuickReader {
 
+    public static int QD = 100;
 
     public static void main(String[] args) {
 
-        for (FileTooReadData s : BenchmarkFiles.filesTooRead) {
-            System.out.println(s.sPath());
-        }
+    }
 
+    public MemorySegment openfile(String path){
+        return null;
+    }
+
+    public void closeFile(MemorySegment seg){
+
+    }
+
+    public void SetupContext(){
+
+    }
+
+    public MemorySegment readFile(MemorySegment fd){
+
+        // queue many things
+
+        // read many things
+
+        return null;
     }
 }
